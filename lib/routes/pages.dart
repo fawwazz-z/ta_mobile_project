a010cf68e917d8a961e3416dfe714e041c3d487d
ASenoJati <senojati16@gmail.com>
2026-04-28 09:37:22 +0700

    Feat [SEN] fragment

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:ta_mobile_project/bindings/loginBinding.dart';
 import 'package:ta_mobile_project/pages/loginPage.dart';
+import 'package:ta_mobile_project/pages/mainPage.dart';
 import 'package:ta_mobile_project/routes/route.dart';
 
 class AppPage {
@@ -10,5 +11,6 @@ class AppPage {
       page: () => LoginPage(),
       binding: LoginBinding(),
     ),
+    GetPage(name: AppRoutes.mainPage, page: () => MainPage()),
   ];
 }
